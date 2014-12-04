5b07328e41663556a8a4511d71dca68f561abb13
Jai-Prakash-Singh <jp213@ymail.com>
2014-12-04 13:52:05 +0530

    first commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
+from django.contrib import admin
+admin.autodiscover()
+ 
+urlpatterns = patterns('',
+     #url(r'^(/)?$', RedirectView.as_view(url='/chats/')),
+     #url(r'^chats/', include('chat.urls')),
+     url(r'^(/)?$', RedirectView.as_view(url='/polls/')),
+     url(r'^polls/', include('polls.urls', namespace="polls")),
+     url(r'^admin/', include(admin.site.urls)),
+)
+
+
