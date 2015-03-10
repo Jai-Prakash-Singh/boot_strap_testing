c06a3c5e6e28a7a94ada94b83c976c907429fa4d
Jai-Prakash-Singh <jp213@ymail.com>
2015-03-10 17:26:05 +0530

    radish tut 1-2

---
+++ rest_frame_work_tutorials/tutorial/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/keshu/boot_strap_testing/rest_frame_work_tutorials/tutorial/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
